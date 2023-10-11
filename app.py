9fc57cde53662490315640caa5d26b381bcdbfa9
manojgantha <89766513+manojgantha@users.noreply.github.com>
2023-10-11 12:14:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,4 +20,4 @@ def get_hit_count():
 @app.route('/')
 def hello():
     count = get_hit_count()
-    return 'Hello Docker! I have been seen {} times.\n'.format(count)
+    return 'Welcome to python code! I have been seen {} times.\n'.format(count)
